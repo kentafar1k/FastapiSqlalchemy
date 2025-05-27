a870b215b86df9456b7a7257d09b22b6de025d23
kentafar1k <129420782+kentafar1k@users.noreply.github.com>
2025-05-27 15:54:55 +0300

    сырые запросы в бд

--- src/database.py
+++ src/database.py
@@ -1,10 +1,36 @@
 import asyncio
 from typing import Annotated
 
-from sqlalchemy import String, create_engine
+from sqlalchemy import String, create_engine, text
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 from sqlalchemy.orm import DeclarativeBase, Session, sessionmaker
 
 from config import settings
 
 
+sync_engine = create_engine(
+    url=settings.DATABASE_URL_psycopg,
+    echo=False,
+    pool_size=5,
+    max_overflow=10,
+)
+
+async_engine = create_async_engine(
+    url=settings.DATABASE_URL_asyncpg,
+    echo=False,
+    pool_size=5,
+    max_overflow=10,
+)
+
+# with sync_engine.connect() as conn:
+#     res = conn.execute(text("SELECT VERSION()"))
+#     print(f"{res.first()=}")
+
+
+async def get_123():
+
+    async with async_engine.connect() as conn:
+        res = await conn.execute(text("SELECT 1,2,3 union select 4,5,6"))
+        print(f"{res.first()=}")
+
+asyncio.run(get_123())
