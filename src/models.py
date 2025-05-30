9fe39d6c8262005e21af8298529a1e788786ee60
kentafar1k <129420782+kentafar1k@users.noreply.github.com>
2025-05-30 14:33:15 +0300

    добавил модель резюме и рабочей нагрузки

--- src/models.py
+++ src/models.py
@@ -1,4 +1,19 @@
-from sqlalchemy import Table, Column, Integer, String, MetaData
+import enum
+
+from sqlalchemy import (
+    TIMESTAMP,
+    CheckConstraint,
+    Column,
+    Enum,
+    ForeignKey,
+    Index,
+    Integer,
+    MetaData,
+    PrimaryKeyConstraint,
+    String,
+    Table,
+    text,
+)
 from sqlalchemy.orm import Mapped, mapped_column
 from src.database import Base
 
@@ -9,6 +24,23 @@ class WorkersOrm(Base):
 
     id: Mapped[int] = mapped_column(primary_key=True)
     username: Mapped[str]
+
+class Workload(enum.Enum):
+    parttime = "parttime"
+    fulltime = "fulltime"
+
+
+class ResumesOrm(Base):
+    __tablename__ = "resumes"
+
+    id: Mapped[intpk]
+    title: Mapped[str]
+    compensation: Mapped[Optional[int]]
+    workload: Mapped[Workload]
+    worker_id: Mapped[int] = mapped_column(ForeignKey("workers.id", ondelete="CASCADE"))
+    created_at: Mapped[created_at]
+    updated_at: Mapped[updated_at]
+
 
 
 
